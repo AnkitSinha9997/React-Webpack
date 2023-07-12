5a2b97455d4d1e746572a93be662db4d3d4d1da8
AnkitSinha9997 <sinhaankit234@gmail.com>
2023-07-12 17:33:14 +0530

    react-webpack-commit

---
+++ src/Components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import landscape from '../assets/images/landscape.jpg';
+import './App.css';
+
+export const App = () => {
+  return (
+    <>
+      <div className="welcome">Welcome to App</div>
+      <img src={landscape} alt="test-image" />
+    </>
+  );
+};
---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const path=require("path");
+const HTMLWebpackPlugin=require("html-webpack-plugin")
+
+module.exports={
+    entry:"/src/index.js",
+    output:{
+        path:path.join(__dirname,"/dist"),
+        filename:"bundle.[hash].js"
+    },
+    plugins:[
+        new HTMLWebpackPlugin({
+            template:"./src/index.html"
+        }),
+
+    ],
+    module:{
+rules: [
+    {
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env','@babel/preset-react']
+        }
+      }
+    },
+    {
+        test: /\.(jpg|png|svg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[hash].[ext]',
+              outputPath: 'images/',
+            },
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        enforce: 'pre',
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader',
+        options: {
+          emitWarning: true, // Emit warnings (optional)
+          formatter: 'eslint-html-reporter',
+          formatterOptions: {
+            outputFile: 'eslint-report.html' // Output file for the HTML report
+          }
+        }
+      },
+  ]
+    }
+}
